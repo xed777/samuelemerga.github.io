a4f7aef59489d3b8e1fa43c40bebe9f836fcd78c
xed <samuelemerga4@gmail.com>
2025-05-22 21:33:50 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const {} = require("tailwindcss/defaultTheme");
-
-module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  variants: {
-    extend: {},
-  },
-};
