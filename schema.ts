61999932dddfdcbb63972a1bd99c08ec50f7369c
xed <samuelemerga4@gmail.com>
2025-05-22 21:33:20 +0200

    Delete schema.ts

--- schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema } from "convex/server";
-import { authTables } from "@convex-dev/auth/server";
-
-const applicationTables = {};
-
-export default defineSchema({
-  ...authTables,
-  ...applicationTables,
-});
